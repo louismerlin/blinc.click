5442e7196f059dbdc35e3de43688fb5bf084d6e8
Louis Merlin <louis.merlin@epfl.ch>
2017-10-12 16:54:40 +0200

    Import bulma css

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,8 @@
-export default () => <h1>Hello, World</h1>
+import stylesheet from 'styles/index.scss'
+export default () => (
+  <main>
+  <style dangerouslySetInnerHTML={{ __html: stylesheet }} />
+  <h1 className="title">Hello, World</h1>
+  <button className="button">hello, button</button>
+  </main>
+)
