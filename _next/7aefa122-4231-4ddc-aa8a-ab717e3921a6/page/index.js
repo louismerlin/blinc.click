192079aef23efdcc87e0e10f437ad4050b7106c1
Louis Merlin <louis.merlin@epfl.ch>
2017-10-26 10:03:31 +0200

    Multiple improvements, add info, navbar, modularize tables

--- _next/7aefa122-4231-4ddc-aa8a-ab717e3921a6/page/index.js
+++ _next/7aefa122-4231-4ddc-aa8a-ab717e3921a6/page/index.js
@@ -1,6 +1,6 @@
 
           window.__NEXT_REGISTER_PAGE('/', function() {
-            var comp = module.exports=webpackJsonp([2],[],[462]);
+            var comp = module.exports=webpackJsonp([2],[],[463]);
             return { page: comp.default }
           })
         
