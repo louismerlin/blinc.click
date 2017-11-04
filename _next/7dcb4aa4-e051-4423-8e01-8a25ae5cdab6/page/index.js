fcc26c139d3e469efe1b990775097549cc7d7720
Louis Merlin <louis.merlin@epfl.ch>
2017-11-04 17:06:47 +0100

    Add google analytics

--- _next/7dcb4aa4-e051-4423-8e01-8a25ae5cdab6/page/index.js
+++ _next/7dcb4aa4-e051-4423-8e01-8a25ae5cdab6/page/index.js
@@ -1,6 +1,6 @@
 
           window.__NEXT_REGISTER_PAGE('/', function() {
-            var comp = module.exports=webpackJsonp([2],[],[465]);
+            var comp = module.exports=webpackJsonp([2],[],[466]);
             return { page: comp.default }
           })
         
