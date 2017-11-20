f70f4c8da6207ebc8f145d5600845787a5c8c781
Louis Merlin <louis.merlin@epfl.ch>
2017-11-20 10:59:38 +0100

    Run next export

--- out/_next/d3aca851-f293-42cd-b934-8781a4b53fba/page/index.js
+++ out/_next/d3aca851-f293-42cd-b934-8781a4b53fba/page/index.js
@@ -1,6 +1,6 @@
 
           window.__NEXT_REGISTER_PAGE('/', function() {
-            var comp = module.exports=webpackJsonp([2],[],[465]);
+            var comp = module.exports=webpackJsonp([2],[],[464]);
             return { page: comp.default }
           })
         
